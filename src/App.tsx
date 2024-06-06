4c938f716e4b91c4506cb34644f124ed5cbb6edf
Gabriel CM <gabriel.moura@einstein.com>
2024-06-06 19:50:10 -0300

    chore: project boilerplate w/ Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -5,6 +6,15 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+=======
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+>>>>>>> 0655ef7 (chore: project boilerplate w/ Vite)
 
   return (
     <>
@@ -29,7 +39,14 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
+<<<<<<< HEAD
   );
 }
 
 export default App;
+=======
+  )
+}
+
+export default App
+>>>>>>> 0655ef7 (chore: project boilerplate w/ Vite)
